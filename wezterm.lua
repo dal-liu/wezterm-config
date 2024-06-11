c074f2153344a10893a867a169c9a4812e9618dd
Daniel <daniel.albert.liu@gmail.com>
2024-06-11 00:12:39 -0500

    changes

--- wezterm.lua
+++ wezterm.lua
@@ -1,21 +1,15 @@
 local wezterm = require("wezterm")
 
-local mux = wezterm.mux
-
 local config = wezterm.config_builder()
 
 config.color_scheme = "Apple System Colors"
-
-config.hide_tab_bar_if_only_one_tab = true
-
-config.use_fancy_tab_bar = true
 
 config.window_frame = {
 	font_size = 14.0,
 }
 
 config.colors = {
-	scrollbar_thumb = "#333333",
+	scrollbar_thumb = "#c0c0c0",
 
 	tab_bar = {
 		active_tab = {
@@ -26,10 +20,10 @@ config.colors = {
 }
 
 config.window_padding = {
-	left = 8,
+	left = 4,
 	right = 14,
-	top = 8,
-	bottom = 8,
+	top = 4,
+	bottom = 4,
 }
 
 config.font = wezterm.font("JetBrainsMono Nerd Font")
@@ -44,5 +38,7 @@ wezterm.on("update-status", function(window, pane)
 	overrides.enable_scroll_bar = dimensions.scrollback_rows > dimensions.viewport_rows
 	window:set_config_overrides(overrides)
 end)
+
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 
 return config
