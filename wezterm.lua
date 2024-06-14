8cbb14c0ab9a47461bac9e439f4f946e24a4e127
Daniel <daniel.albert.liu@gmail.com>
2024-06-14 12:50:31 -0500

    some changes

--- wezterm.lua
+++ wezterm.lua
@@ -4,6 +4,8 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "Apple System Colors"
 
+config.use_fancy_tab_bar = false
+
 config.hide_tab_bar_if_only_one_tab = true
 
 config.window_frame = {
@@ -28,7 +30,7 @@ config.window_padding = {
 	bottom = 4,
 }
 
-config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font = wezterm.font_with_fallback({ "JetBrainsMono Nerd Font", "JetBrains Mono" })
 
 config.font_size = 16.0
 
